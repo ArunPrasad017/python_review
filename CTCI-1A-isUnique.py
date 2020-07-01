bffc3f8f89d86c5aebbe568186e276bed3568ad1
aralagarsamy <aralagarsamy@egencia.com>
2020-06-30 22:43:43 -0700

    practice-1

---
+++ CTCI-1A-isUnique.py
@@ -0,0 +1,14 @@
+def isUnique(string):
+    if string == "" or len(string)==1:
+        return True
+    hash_set=set()
+    for c in string:
+        if c not in hash_set:
+            hash_set.add(c)
+        else:
+            return False
+    return True
+
+print(isUnique(""))
+print(isUnique("abcde"))
+print(isUnique("abcda"))
