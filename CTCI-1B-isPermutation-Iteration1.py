0362160d031615414895c909a6cbd178e0223c0b
Sourcery AI <>
2020-07-27 06:30:18 +0000

    'Refactored by Sourcery'

--- CTCI-1B-isPermutation-Iteration1.py
+++ CTCI-1B-isPermutation-Iteration1.py
@@ -1,8 +1,5 @@
 def isPermutation(s1,s2):
-    if sorted(s1) == sorted(s2):
-        return True
-    else:
-        return False
+    return sorted(s1) == sorted(s2)
 
 print(isPermutation("abc", "bac"))
 print(isPermutation("aabbc", "bcaba"))
